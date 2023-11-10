deba9c22dc3f72ae68438e9d277b8420930b8261
soybean15 <marlonpadilla1593@gmail.com>
2023-11-10 21:24:11 +0800

    finished add subject

--- src/views/admin/components/SubjectPanel.vue
+++ src/views/admin/components/SubjectPanel.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="w-full  relative  h-full" >
-    <q-fab
-        v-model="fabLeft"
-        class="absolute bottom-10 right-10"
-        vertical-actions-align="right"
-        color="primary"
-        icon="keyboard_arrow_up"
-        direction="up"
-      >
   
-        <q-fab-action label-position="right" color="primary" @click="onClick" icon="mail" label="Email" />
-     
-        <q-fab-action label-position="right" color="secondary" @click="onClick" icon="alarm" label="Alarm" />
-        <q-fab-action label-position="right" color="orange" @click="onClick" icon="airplay" label="Airplay" />
-        <q-fab-action label-position="right" color="accent" @click="dialog.add = true" icon="room" label="Map" />
-      </q-fab>
+    <q-btn  @click="dialog.add = true"  class="absolute bottom-10 right-10" fab icon="add" color="accent" />
 
      <AddSubjects/>
 
