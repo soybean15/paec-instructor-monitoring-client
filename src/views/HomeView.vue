6a5600fc676b329adf34ac5c305abb8455eb0e64
soybean15 <marlonpadilla1593@gmail.com>
2023-11-14 20:21:16 +0800

    fix camera

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="q-px-lg q-py-md ">
-   <Camera/>
+
   </div>
 </template>
 <script>
@@ -8,12 +8,11 @@ import { useAuthStore } from '@/store/auth';
 import { storeToRefs } from 'pinia';
 import { onMounted } from 'vue';
 
-import Camera from './components/Camera.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    Camera
+    
   },
   setup(){
    
