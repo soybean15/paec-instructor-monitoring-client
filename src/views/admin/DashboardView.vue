f2f9522d987a45bc7d0b3a3d4110e1fd0d397f20
soybean15 <marlonpadilla1593@gmail.com>
2023-12-03 18:56:42 +0800

    added dash board

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -6,6 +6,15 @@
         <div class="flex text-lg font-secondary"> Welcome back,  <span class="font-bold px-2">{{user.profile.firstname}}</span></div>
 
 
+        <div class="row ">
+            <stat class=" col-6 col-md-3" :title="'Total Users'" :value="30"/>
+
+             <stat class="col-6 col-md-3" :title="'Teachers'" :value="30"/>
+             <stat class="col-6 col-md-3" :title="'Pending'" :value="30"/>
+             <stat class="col-6 col-md-3" :title="'Course'" :value="30"/> 
+             <stat class="col-6 col-md-3" :title="'Subjects'" :value="30"/>
+             <stat class="col-6 col-md-3" :title="'Departments'" :value="30"/>
+        </div>
 
     </div>
    
@@ -14,8 +23,9 @@
 <script>
 import { useUserStore } from '@/store/user'
 import { storeToRefs } from 'pinia'
+import Stat from '@/components/Stat.vue'
 export default {
-
+components:{Stat},
     setup(){
         const userStore = useUserStore()
 
