365f3fcfb51aa4bb1c39a5ddc29f9b97a5c5f7d5
soybean15 <marlonpadilla1593@gmail.com>
2023-12-03 20:07:42 +0800

    finished simple dashboard

--- src/store/dashboard.js
+++ src/store/dashboard.js
@@ -1,12 +1,25 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 
-export  const useDashboardStore = defineStore(()=>{
+export  const useDashboardStore = defineStore('dashboard',()=>{
 
 
     const dashboard = ref(null)
 
+    const index = async()=>{
+        const response =await axios.get('api/admin/dashboard')
+        dashboard.value = response.data
+    
 
+    }
+
+
+
+    return{
+        index,
+        dashboard
+    }
 
 })
--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -6,17 +6,20 @@
         <div class="flex text-lg font-secondary"> Welcome back,  <span class="font-bold px-2">{{user.profile.firstname}}</span></div>
 
 
-        <div class="row ">
+        <div class="row my-10"  v-if="dashboard">
             <stat class=" col-6 col-md-3" :title="'Total Users'" :value="30"/>
 
-             <stat class="col-6 col-md-3" :title="'Teachers'" :value="30"/>
-             <stat class="col-6 col-md-3" :title="'Pending'" :value="30"/>
-             <stat class="col-6 col-md-3" :title="'Course'" :value="30"/> 
-             <stat class="col-6 col-md-3" :title="'Subjects'" :value="30"/>
-             <stat class="col-6 col-md-3" :title="'Departments'" :value="30"/>
+             <stat class="col-6 col-md-3" :title="'Teachers'" :value="dashboard.teacher_count"/>
+             <stat class="col-6 col-md-3" :title="'Pending'" :value="dashboard.teacher_pending_count"/>
+             <stat class="col-6 col-md-3" :title="'Course'" :value="dashboard.course_count"/> 
+             <stat class="col-6 col-md-3" :title="'Subjects'" :value="dashboard.subject_count"/>
+             <stat class="col-6 col-md-3" :title="'Departments'" :value="dashboard.department_count"/>
         </div>
-
+        
+     
     </div>
+
+   
    
 </template>
 
@@ -24,14 +27,26 @@
 import { useUserStore } from '@/store/user'
 import { storeToRefs } from 'pinia'
 import Stat from '@/components/Stat.vue'
+import { useDashboardStore } from '@/store/dashboard'
+import { onMounted } from 'vue'
 export default {
 components:{Stat},
     setup(){
         const userStore = useUserStore()
 
         const {user} = storeToRefs(userStore)
+
+        const  dashboardStore = useDashboardStore()
+
+        const {dashboard}= storeToRefs(dashboardStore)
+        onMounted(()=>{
+
+            dashboardStore.index()
+
+        })
         return {
-            user
+            user,
+            dashboard
         }
     }
 }
